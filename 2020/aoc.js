4487959f497a6a2999c9956bc6ebd1db9fc8ae1b
jacknight <jacknight@gmail.com>
2020-12-03 07:28:41 +0200

    tidy js

---
+++ 2020/aoc.js
@@ -0,0 +1,22 @@
+function displayAnswer(answer) {
+  var answerDiv = document.createElement("div");
+  answerDiv.classList = "answer";
+  answerDiv.textContent = "Answer: " + answer;
+  document.body.appendChild(answerDiv);
+
+  if (answer !== undefined) {
+    var answerCopyButton = document.createElement("button");
+    answerCopyButton.textContent = "Copy to clipboard";
+    answerCopyButton.classList = "answer-button";
+    document.querySelector(".answer").appendChild(answerCopyButton);
+
+    answerCopyButton.onclick = function () {
+      const el = document.createElement("textarea");
+      el.value = answer;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand("copy");
+      document.body.removeChild(el);
+    };
+  }
+}
